f4619a1fa3bfcec79afcfe943fd15a42793dadcb
haki24gamer <haguimohamed456@gmail.com>
2023-09-27 16:22:05 +0300

    new file:   Leetcode/C/Two_Sum new file:   Leetcode/C/Two_Sum.c modified:   Leetcode/Python/Two_Sum.py

---
+++ Leetcode/C/Two_Sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
+
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    
+}
--- Leetcode/Python/Two_Sum.py
+++ Leetcode/Python/Two_Sum.py
@@ -1,3 +1,21 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+"""
+run through the array nums, and add the the numbers together until finding a result equivalent to the target
+"""
 class Solution(object):
-    def twoSum(self, nums, target):
-        print("Hello, World!")
+    def twoSum():
+        nums = [3,3]
+        target = 6
+        n = len(nums)
+
+
+        for i in range(n):
+            for j in range(i+1,n):
+                sum = nums[i] + nums[j]
+                if sum==target:
+                    a=i
+                    b=j
+        
+        print("[",a,",",b,"]")
+Solution.twoSum()
+
