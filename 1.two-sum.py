5b071d0610ebcc1128040806b97769cf0c82dd1e
haki24gamer <haguimohamed456@gmail.com>
2023-09-29 15:59:13 +0300

    new file:   1.two-sum.py deleted:    Leetcode/Easy/C/Two_Sum deleted:    Leetcode/Easy/C/Two_Sum.c deleted:    Leetcode/Easy/Python/Palindrome.py deleted:    Leetcode/Easy/Python/Roman_to_Integer.py deleted:    Leetcode/Easy/Python/Two_Sum.py

---
+++ 1.two-sum.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        print("Hello, World")
+    
+Solution().twoSum(5,3)
+# @lc code=end
+
