b5f51c4b481f4c0a280f19a98611b33b2fe9f6b8
haki24gamer <haguimohamed456@gmail.com>
2024-01-14 19:59:36 +0300

    modified:   C/Playground/rev.c

--- C/Playground/rev.c
+++ C/Playground/rev.c
@@ -8,17 +8,17 @@ void inverser(char tableau[]){
     {
         tableauInv[i]=tableau[n-i-1];
     }
-    printf("%s",tableauInv);
+    printf("%s\n",tableauInv);
 }
 
 void afficher(char tableau[]){
-    printf("%s",tableau);
+    printf("%s\n",tableau);
 }
 
 void palindrome(char tableau[]){
     int res=0;
     int n = strlen(tableau);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n/2; i++)
     {
         if (tableau[i]!=tableau[n-1-i])
         {
@@ -36,16 +36,77 @@ void palindrome(char tableau[]){
     }
 }
 
+int taille(char tableau){
+    int res=strlen(tableau);
+    return res;
+}
+
+int occ(char tab[], char x)
+{
+    int cpt=0;
+    for (int i = 0; i < strlen(tab); i++)
+    {
+        if (tab[i]==x)
+        {
+            cpt++;
+        }
+        
+    }
+    return cpt;
+    
+}
+
+int Comp(char tab1, char tab2){
+    int res = strcmp(tab1,tab2);
+    if (res)
+    {
+        printf("Identique");
+    }
+    else
+    {
+        printf("Pas identique");
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
-    char tab[50];
-    scanf("%s",tab);
+    
+    char tabX[50];
+    printf("Entrer la chaine de caractere:\n");
+    fgets(tabX,50,stdin);
+    scanf("%s",tabX);
+    char a;
+    printf("Entrer le caractere a compter:\n");
+    scanf("%c",&a);
+    for (int i = 0; i > strlen(tabX)-1; i++)
+    {
+        printf("%c",tabX[i]);
+    }
+    
+    char tabX[]="haki";
+    char tabY[]="hki";
+    
+    int nbrDeCaractere = strlen(tabX);
+    printf("%d",nbrDeCaractere);
+    int comparaison = strcmp(tabX,tabY);
+    printf("%d",comparaison);
+    int taille = sizeof(tabX);
+    printf("%d",taille);
+    strcpy(tabX,tabY);
+    strcat(tabX,tabY);
+    
+    
+    // char tabY[50];
+    // fgets(tabY,50,stdin);
     // printf("%s \n",tab);
 
     // inverser(tab);
     // afficher(tab);
-    //palindrome(tab);
-    
+    // palindrome(tab); 
+    //Comp(tabX,tabY);
+    // int res = occ(tabX,a);
+    // printf("%c se repete %d fois dans %s\n",a,res,tabX);
+
     return 0;
 }
