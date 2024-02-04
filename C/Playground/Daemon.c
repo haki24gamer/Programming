14a9076d9b2859cb047038306fcfa99ce932b281
haki24gamer <haguimohamed456@gmail.com>
2024-02-04 12:14:43 +0300

    new file:   Web/Web.code-workspace

---
+++ C/Playground/Daemon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n=1;
+    switch (n)
+    {
+    case 1:
+        printf("0");
+        break;
+    
+    default:
+        printf("What");
+        break;
+    }
+    return 0;
+}
