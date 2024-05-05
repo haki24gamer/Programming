40c33dc8fbab2f520555e6f4294345ca4b3ed025
haki24gamer <haguimohamed456@gmail.com>
2024-05-05 19:39:00 +0300

    new file:   Java/CF/CF2021/Exo1/src/Main.java

---
+++ Java/CF/CF2021/Exo1/src/Auto_Ecole.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Auto_Ecole {
+    protected int nb;
+    protected Vehicule[] tab;
+
+    public Auto_Ecole(int nb){
+        this.nb = nb;
+        tab = new Vehicule[nb];
+    }
+}
---
+++ Java/CF/CF2021/Exo1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String a = "Hehe";
+        String b = "Haha";
+        String c = "Hihi";
+        System.out.println(a+b+c);
+    }
